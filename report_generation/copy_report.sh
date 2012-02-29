acb13a9bbaf74abe82dbf511f10361e1411570e3
Anurag Solanki <anurag@vinsol.com>
2012-02-29 12:23:10 +0530

    Modified after review and used SED to select data from report file

--- report_generation/copy_report.sh
+++ report_generation/copy_report.sh
@@ -3,22 +3,24 @@
 FILE1="subscribers.txt"
 FILE2="yahoo_record.txt"
 FILE3="Chennai_record.txt"
-CHENNAI=" Chennai"
-YAHOO="@yahoo.com"
 
-if [ -e $FILE2 ]; then
-	echo
-else 
-	touch $FILE2
-fi
-if [ -e $FILE3 ]; then
-	echo
-else 
-	touch $FILE3
-fi 
+sed -n 's/ Chennai/&/p' $FILE1 > $FILE3
+sed -n 's/@yahoo.com/&/p' $FILE1 > $FILE2
 
-while read line    
-do    
-	echo $line | grep $CHENNAI >> $FILE3
-	echo $line | grep $YAHOO >> $FILE2
-done <$FILE1
+#if [ -e $FILE2 ]; then
+#	echo
+#else 
+#	touch $FILE2
+#fi
+#if [ -e $FILE3 ]; then
+#	echo
+#else 
+#	touch $FILE3
+#fi 
+
+#while read line    
+#do    
+#	echo $line | grep $CHENNAI >> $FILE3
+#	echo $line | grep $YAHOO >> $FILE2
+#done <$FILE1
+
